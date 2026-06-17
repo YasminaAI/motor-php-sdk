04e35e683c2f06c938d79e5dbe543ce0b7b9353a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-17 11:42:22 +0000

    Release 0.0.51

--- src/YasminaaiClient.php
+++ src/YasminaaiClient.php
@@ -59,8 +59,8 @@ class YasminaaiClient
             'Authorization' => "Bearer $token",
             'X-Fern-Language' => 'PHP',
             'X-Fern-SDK-Name' => 'Yasminaai',
-            'X-Fern-SDK-Version' => '0.0.50',
-            'User-Agent' => 'yasminaai/yasminaai/0.0.50',
+            'X-Fern-SDK-Version' => '0.0.51',
+            'User-Agent' => 'yasminaai/yasminaai/0.0.51',
         ];
 
         $this->options = $options ?? [];
